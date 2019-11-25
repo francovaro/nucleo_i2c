cfacbbb75ab7ab6480b6d99954356692699fa3d0
Francesco <francovaro@gmail.com>
2019-11-25 23:43:55 +0100

    gitignore

--- Debug/src/led_matrix.d
+++
@@ -1,7 +0,0 @@
-src/led_matrix.o: ../src/led_matrix.c \
- C:/work/workspace_stm/nucleo_i2c/inc/led_matrix.h \
- C:/work/workspace_stm/nucleo_i2c/inc/mcp23017.h
-
-C:/work/workspace_stm/nucleo_i2c/inc/led_matrix.h:
-
-C:/work/workspace_stm/nucleo_i2c/inc/mcp23017.h:
--- src/test.c
+++ src/test.c
@@ -7,7 +7,8 @@ void initLed(void)
 	GPIO_InitTypeDef GPIO_InitStructure;
 	GPIO_StructInit(&GPIO_InitStructure);
 
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);  //Per il LED e  canale analogico
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
 	GPIO_InitStructure.GPIO_Pin = ( GPIO_Pin_5 );
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
